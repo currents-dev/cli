e735ba2f2c6a22f45470c3d317852cf198e1cc15
Andrew Goldis <agoldis@gmail.com>
2022-09-02 11:29:43 -0700

    feat: add currents-pre

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const { run } = require('cy2');
+const { run, patch } = require("cy2");
 
 exports.run = async function main() {
-  await run('https://cy.currents.dev', 'currents');
+  await run("https://cy.currents.dev", "currents");
+};
+
+exports.patch = async function () {
+  await patch("https://cy.currents.dev");
 };
