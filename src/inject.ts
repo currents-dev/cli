d69876ad0c8bee763bcf727939ab4e77fb9c69d8
Andrew Goldis <agoldis@gmail.com>
2022-12-06 22:33:23 -0800

    fix: patch function

--- src/inject.ts
+++ src/inject.ts
@@ -3,6 +3,9 @@ const Mod = require("module");
 const req = Mod.prototype.require;
 
 // @ts-ignore
+global.integrityCheck = function () {};
+
+// @ts-ignore
 if (global.snapshotResult?.customRequire?.exports) {
   // @ts-ignore
   global.snapshotResult.customRequire.exports[
@@ -18,6 +21,16 @@ if (global.snapshotResult?.customRequire?.exports) {
 }
 
 Mod.prototype.require = function (...args) {
+  if (args[0].match(/app\.json/)) {
+    return {
+      development: { api_url: "http://localhost:1234/" },
+      test: { api_url: "http://localhost:1234/" },
+      staging: { api_url: "https://api-staging.cypress.io/" },
+      production: {
+        api_url: "https://cy.currents.dev",
+      },
+    };
+  }
   if (args[0] === "konfig") {
     return () => ({
       app: {
